4157bbaa8c4772b5a2974031d9a124b686b612c4
dulcesrecuerdosshop-bit <dulcesrecuerdosshop@gmail.com>
2025-12-02 20:10:56 +0100

    Update CACHE_VERSION to v9 in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker corrected - CACHE v3
-const CACHE_VERSION = 'v8';
+const CACHE_VERSION = 'v9';
 const CACHE_NAME = `llavero-respira-${CACHE_VERSION}`;
 const ASSETS = ['./','index.html','logo.png','helpers.js','manifest.json'];
 
